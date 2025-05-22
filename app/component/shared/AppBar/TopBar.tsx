60cfc9f9275a91daec703d4164d3394afe4b67e2
DonDuala <john@dual.finance>
2025-05-21 22:38:21 -0400

    update app links

--- app/component/shared/AppBar/TopBar.tsx
+++ app/component/shared/AppBar/TopBar.tsx
@@ -41,7 +41,7 @@ const TopBar = () => {
             })}
           </div>
           <Link 
-            href="https://levels-ai.xyz/trading" 
+            href="https://app.manifest.trade" 
             target="_blank" 
             rel="noopener noreferrer"
             className='text-black z-50 px-8 2xl:px-10 3xl:px-16 py-3 bg-[#95C9BD] rounded-lg text-[16px] font-medium hover:opacity-80 active:translate-y-[1px]'>
